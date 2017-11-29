d681b3a627cd3bdb425febb8bb74222558261c39
Ido Schimmel <idosch@mellanox.com>
2017-11-29 18:47:53 +0200

    Add FIB netdev unregister test
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>

---
+++ 006-fib-netdev-unregister.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "SKIP: Need root privileges"
+	exit 0
+fi
+
+source lib.sh
+
+ret=0
+
+shopt -s expand_aliases
+alias ipns='ip netns exec test'
+
+netns_create()
+{
+	ip netns add test
+	ip netns exec test ip link set dev lo up
+}
+
+netns_destroy()
+{
+	ip netns del test
+}
+
+check_err()
+{
+	if [ $ret -eq 0 ]; then
+		ret=$1
+	fi
+}
+
+check_fail()
+{
+	if [ $1 -eq 0 ]; then
+		ret=1
+	fi
+}
+
+dummy_create()
+{
+	ipns ip link add name $1 type dummy
+	ipns ip link set dev $1 up
+}
+
+dc_test()
+{
+	dummy_create "dummy0"
+
+	ipns ip address add 198.51.100.1/24 dev dummy0
+	ipns ip -6 address add 2001:db8:1::1/64 dev dummy0
+
+	ipns ip route get fibmatch 198.51.100.2 &> /dev/null
+	check_err $?
+	ipns ip -6 route get fibmatch 2001:db8:1::2 &> /dev/null
+	check_err $?
+
+	ipns ip link del dev dummy0
+	check_err $?
+
+	ipns ip route get fibmatch 198.51.100.2 &> /dev/null
+	check_fail $?
+	ipns ip -6 route get fibmatch 2001:db8:1::2 &> /dev/null
+	check_fail $?
+
+	if [ $ret -ne 0 ]; then
+		echo "FAIL: directly connected route test"
+		return 1
+	fi
+	echo "PASS: directly connected route test"
+}
+
+remote_test()
+{
+	dummy_create "dummy0"
+
+	ipns ip address add 198.51.100.1/24 dev dummy0
+	ipns ip -6 address add 2001:db8:1::1/64 dev dummy0
+
+	ipns ip route add 192.0.2.0/24 nexthop via 198.51.100.2 dev dummy0
+	ipns ip -6 route add 2001:db8:2::/64 nexthop via 2001:db8:1::2 dev \
+		dummy0
+
+	ipns ip route get fibmatch 192.0.2.1 &> /dev/null
+	check_err $?
+	ipns ip -6 route get fibmatch 2001:db8:2::1 &> /dev/null
+	check_err $?
+
+	ipns ip link del dev dummy0
+	check_err $?
+
+	ipns ip route get fibmatch 192.0.2.1 &> /dev/null
+	check_fail $?
+	ipns ip -6 route get fibmatch 2001:db8:2::1 &> /dev/null
+	check_fail $?
+
+	if [ $ret -ne 0 ]; then
+		echo "FAIL: remote route test"
+		return 1
+	fi
+	echo "PASS: remote route test"
+}
+
+multipath_test()
+{
+	dummy_create "dummy0"
+	dummy_create "dummy1"
+
+	ipns ip address add 198.51.100.1/24 dev dummy0
+	ipns ip -6 address add 2001:db8:1::1/64 dev dummy0
+
+	ipns ip address add 192.0.2.1/24 dev dummy1
+	ipns ip -6 address add 2001:db8:2::1/64 dev dummy1
+
+	ipns ip route add 203.0.113.0/24 \
+		nexthop via 198.51.100.2 dev dummy0 \
+		nexthop via 192.0.2.2 dev dummy1
+	ipns ip -6 route add 2001:db8:3::/64 \
+		nexthop via 2001:db8:1::2 dev dummy0 \
+		nexthop via 2001:db8:2::2 dev dummy1
+
+	ipns ip route get fibmatch 203.0.113.1 &> /dev/null
+	check_err $?
+	ipns ip -6 route get fibmatch 2001:db8:3::1 &> /dev/null
+	check_err $?
+
+	ipns ip link del dev dummy0
+	check_err $?
+
+	ipns ip route get fibmatch 203.0.113.1 &> /dev/null
+	check_fail $?
+	ipns ip -6 route get fibmatch 2001:db8:3::1 &> /dev/null
+	check_fail $?
+
+	ipns ip link del dev dummy1
+
+	if [ $ret -ne 0 ]; then
+		echo "FAIL: multipath route test"
+		return 1
+	fi
+	echo "PASS: multipath route test"
+}
+
+setup_prepare()
+{
+	netns_create
+}
+
+cleanup()
+{
+	netns_destroy
+}
+
+trap cleanup EXIT
+
+setup_prepare
+
+dc_test
+remote_test
+multipath_test
+
+exit $ret
